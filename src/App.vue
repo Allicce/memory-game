9d6033f60780fe8eb49bd3f806569e1e71931b59
Alžbeta <stetiarova.alzbeta@gmail.com>
2019-11-27 22:09:00 +0100

    router to Game Screen / fix eslint

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Welcome Page</router-link> |
+      <router-link to="/gameScreen">Game Screen</router-link>
     </div>
     <router-view/>
   </div>
---
+++ src/components/GameScreen.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <Timer></Timer>
+  </div>
+
+</template>
+
+<script>
+    import Timer from '../views/Timer.vue'
+
+    export default {
+        name: "GameScreen",
+        components: {
+            'Timer': Timer
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
