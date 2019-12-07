4ed421c6842ae3a4a6ec8a877989a0ab1a606111
Alžbeta <stetiarova.alzbeta@gmail.com>
2019-12-07 15:09:48 +0100

    add buttons + v-show if resalts is ok or wrong

--- src/views/Results.vue
+++ src/views/Results.vue
@@ -1,25 +1,40 @@
 <template>
   <div>
     <Header/>
-    <h1>Results</h1>
+    <div v-show="isEqual">
+      <h1>Správne</h1>
+    </div>
+    <div v-show="!isEqual">
+      <h1>Chyba</h1>
+    </div>
     <div v-for="(number, index) in watchNumber" :key="index"> {{ number }}</div>
     <hr>
     <div v-for="(number, index) in usersNumber" :key="index"> {{ number }}</div>
+    <div v-show="isEqual">
+      <Button class="button" name="pokračuj" routeTo="/gameScreen"/>
+    </div>
+    <div v-show="!isEqual">
+      <Button class="button" name="hraj znova!" routeTo="/gameScreen"/>
+    </div>
   </div>
 </template>
 
 <script>
 import { mapState } from 'vuex'
 import Header from '../components/Header'
+import _ from 'lodash'
+import Button from '../components/Button'
 
 export default {
   name: 'Results',
   components: {
-    Header
+    Header,
+    Button
   },
   data () {
     return {
-      number: ''
+      number: '',
+      isEqual: true
     }
   },
   computed: {
@@ -27,6 +42,9 @@ export default {
       'watchNumber',
       'usersNumber'
     ])
+  },
+  created () {
+    this.isEqual = _.isEqual(this.watchNumber, this.usersNumber)
   }
 }
 </script>
