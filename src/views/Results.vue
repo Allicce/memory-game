9701a8498738d8d7b4c946ba6b754bc6956620c1
Alžbeta <stetiarova.alzbeta@gmail.com>
2019-12-09 21:12:48 +0100

    resul page design mobile + pc

--- src/views/Results.vue
+++ src/views/Results.vue
@@ -7,25 +7,33 @@
     <div v-show="!isEqual">
       <h1 class="mistake">Chyba</h1>
     </div>
+    <p>Zobrazené čísla</p>
     <div class="container">
       <div v-for="(number, index) in watchNumber" :key="index" class="number"> {{ number }}</div>
     </div>
+    <p>Tvoje čísla</p>
     <div class="container">
       <div v-for="(number, index) in usersNumber" :key="index" class="number"> {{ number }}</div>
     </div>
 
-    <div v-show="isEqual">
+    <div v-show="isEqual" class="button_container">
       <Button class="button margin-5"
               name="pokračuj"
               @click.native="goToPath('/gameScreen'), nextLevel()"
 
       />
+      <div class="image">
+        <img src="../../public/Charac_F-F2-Jump.gif">
+      </div>
     </div>
-    <div v-show="!isEqual">
+    <div v-show="!isEqual" class="button_container">
       <Button class="button"
               name="hraj znova!"
               @click.native="goToPath('/gameScreen'), resetToFirstLevel()"
       />
+      <div class="image">
+        <img src="../../public/Characs_F-F2-Idle.gif">
+      </div>
     </div>
   </div>
 </template>
@@ -96,19 +104,25 @@ export default {
 
 <style scoped>
   .container {
+    width: 90vw;
     margin: auto;
-    width: 80vw;
-    display: flex;
-    justify-content: space-between;
+    display: grid;
+    grid-template-columns: repeat(2, minmax(100px, 1fr));
+    justify-content: center;
+    padding-top: 5%;
   }
 
   .number {
-    margin-top: 10%;
+    justify-self: center;
+    min-width: 30vw;
+    max-width: 30vw;
     background-color: #041F1E;
-    color: white;
-    padding: 0 20px;
-    font-size: 50px;
+    padding: 0 10px;
     border-radius: 5%;
+    color: white;
+    text-align: center;
+    font-size: 40px;
+    margin: 3% 3% 0 0;
   }
 
   .mistake {
@@ -118,6 +132,49 @@ export default {
   .margin-5 {
     margin-top: 5%;
     padding: 0;
+  }
+
+  .image{
+    display: none;
+  }
+
+  @media(min-width: 700px) {
+    .container {
+      width: 70vw;
+      padding: 3%;
+      grid-template-columns: repeat(3, minmax(80px, 1fr));
+      font-size: 40px;
+    }
+
+    .number {
+      min-width: 15vw;
+      max-width: 15vw;
+      padding: 0 10px;
+      font-size: 1.5em;
+    }
+
+    .button {
+      width: 15vw;
+      padding: 3% 10%;
+
+    }
+
+    .image {
+      display: block;
+      width: 15vw;
+    }
+
+    .image  img {
+      width: 100%;
+    }
+
+  .button_container {
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    padding: 0 5%;
+  }
+
   }
 
 </style>
