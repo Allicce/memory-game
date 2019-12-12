42278ab2d031a9bfea6cd0f7052fc9482c838ae8
Alžbeta <stetiarova.alzbeta@gmail.com>
2019-12-12 18:59:06 +0100

    dizajn result

--- src/components/DragAndDrop.vue
+++ src/components/DragAndDrop.vue
@@ -1,25 +1,28 @@
 <template>
   <div class="container">
-    <div v-for="(list, i) in lists" :key="i">
-      <drop class="drop list" @drop="handleDrop(list, ...arguments)">
-        <drag v-for="(item, index) in list"
-              class="drag"
-              :key="index"
-              :class="{ [item]: true }"
-              :transfer-data="{ item: item, list: list, example: 'lists' }">
-        </drag>
-      </drop>
+    <div class="container_drag grid">
+      <div v-for="(list, i) in lists" :key="i">
+        <drop class="drop list smaller" @drop="handleDrop(list, ...arguments)">
+          <drag v-for="(item, index) in list"
+                class="drag"
+                :key="index"
+                :class="{ [item]: true }"
+                :transfer-data="{ item: item, list: list, example: 'lists' }">
+          </drag>
+        </drop>
+      </div>
     </div>
-    <div v-for="(list, i) in userResults" :key="i">
-      <drop class="drop list" @drop="handleDrop(list, ...arguments)">
-        <drag v-for="(item, index) in list"
-              class="drag"
-              :key="index"
-              :class="{ [item]: true }"
-              :transfer-data="{ item: item, list: list, example: 'lists' }">
-        </drag>
-      </drop>
-
+    <div class="container_drag bigger space">
+      <div v-for="(list, i) in userResults" :key="i">
+        <drop class="drop list" @drop="handleDrop(list, ...arguments)">
+          <drag v-for="(item, index) in list"
+                class="drag"
+                :key="index"
+                :class="{ [item]: true }"
+                :transfer-data="{ item: item, list: list, example: 'lists' }">
+          </drag>
+        </drop>
+      </div>
     </div>
   </div>
 
@@ -77,13 +80,23 @@ export default {
 </script>
 
 <style scoped>
+  .container {
+    width: 60vw;
+    display: flex;
+    margin: auto;
+    align-items: center;
+  }
+
+  .container_drag {
+    flex: 1 1 50%;
+  }
 
   .drag, .drop {
     box-sizing: border-box;
-    display: inline-block;
+    display: block;
     border-radius: 10px;
-    width: 100px;
-    height: 100px;
+    width: 70px;
+    height: 70px;
     background: #ccc;
     vertical-align: middle;
     margin-right: 20px;
@@ -131,15 +144,28 @@ export default {
     display: inline-block;
     vertical-align: top;
     padding: 15px;
-    margin-bottom: 20px;
+    margin: 20px 20px;
     width: auto;
     height: auto;
   }
 
   .list {
-    min-width: 100px;
-    min-height: 100px;
+    min-width: 110px;
+    min-height: 110px;
     background-color: #041F1E;
+  }
+
+  .bigger {
+    display: grid;
+    grid-template-columns: repeat(3, minmax(140px, 1fr));
+    justify-content: space-around;
+  }
+
+  .smaller {
+    display: grid;
+    grid-template-columns: repeat(3, minmax(80px, 1fr));
+    justify-content: space-around;
+
   }
 
 </style>
