fc69242c1409819f66922d56480098bb0aba96d1
Alžbeta <stetiarova.alzbeta@gmail.com>
2019-12-15 11:18:17 +0100

    add chart

---
+++ src/components/Chart.vue
@@ -0,0 +1,64 @@
+<script>
+import { Line } from 'vue-chartjs'
+
+export default {
+  extends: Line,
+  data () {
+    return {
+      gradient: null,
+      gradient2: null
+    }
+  },
+  mounted () {
+    this.gradient = this.$refs.canvas
+      .getContext('2d')
+      .createLinearGradient(0, 0, 0, 450)
+    this.gradient2 = this.$refs.canvas
+      .getContext('2d')
+      .createLinearGradient(0, 0, 0, 450)
+
+    this.gradient.addColorStop(0, 'rgba(255, 0,0, 0.5)')
+    this.gradient.addColorStop(0.5, 'rgba(255, 0, 0, 0.25)')
+    this.gradient.addColorStop(1, 'rgba(255, 0, 0, 0)')
+
+    this.gradient2.addColorStop(0, 'rgba(0, 231, 255, 0.9)')
+    this.gradient2.addColorStop(0.5, 'rgba(0, 231, 255, 0.25)')
+    this.gradient2.addColorStop(1, 'rgba(0, 231, 255, 0)')
+
+    this.renderChart(
+      {
+        labels: [
+          'Lev 1-3',
+          'Lev 4-6',
+          'Lev 7-9',
+          'Lev 10-12',
+          'Lev 13-15',
+          'Lev 16-18',
+          'Lev 19-20'
+        ],
+        datasets: [
+          {
+            label: 'Data One',
+            borderColor: '#FC2525',
+            pointBackgroundColor: 'white',
+            borderWidth: 1,
+            pointBorderColor: 'white',
+            backgroundColor: this.gradient,
+            data: [80, 220, 100, 50, 85, 20, 12]
+          } /* ,
+          {
+            label: 'Data Two',
+            borderColor: '#05CBE1',
+            pointBackgroundColor: 'white',
+            pointBorderColor: 'white',
+            borderWidth: 1,
+            backgroundColor: this.gradient2,
+            data: [60, 55, 32, 10, 2, 12, 53]
+          } */
+        ]
+      },
+      { responsive: true, maintainAspectRatio: false }
+    )
+  }
+}
+</script>
--- src/views/Percentil.vue
+++ src/views/Percentil.vue
@@ -1,27 +1,102 @@
 <template>
 
-  <div>
-    <h1> {{percentil}} percentil</h1>
+  <div class="containter">
+    <h1>Percentil: {{percentil}}</h1>
+    <h2>Si lepší ako {{percentil}}% populácie</h2>
+    <div class="chart">
+      <chart-area></chart-area>
+    </div>
+
+    <div class="conainer_button">
+      <Buttons name="Hraj znova" @click.native="goToPath('/gameScreen')"></Buttons>
+    </div>
   </div>
 
 </template>
 
 <script>
 import { mapState } from 'vuex'
+import Buttons from '../components/Button.vue'
+import ChartArea from '../components/Chart.vue'
+
 export default {
   name: 'Percentil',
-  props: {
-    percentil: Number
+  components: {
+    Buttons,
+    ChartArea
+  },
+  data () {
+    return {
+      percentil: 1
+    }
   },
   computed: {
     ...mapState([
       'actualLevel'
     ])
+  },
+  methods: {
+    addPercentil () {
+      switch (this.actualLevel) {
+        case 1: {
+          this.percentil = 1
+          break
+        }
+        case 2: {
+          this.percentil = 3
+          break
+        }
+        case 3: {
+          this.percentil = 20
+          break
+        }
+        case 4: {
+          this.percentil = 25
+          break
+        }
+        case 5: {
+          this.percentil = 30
+          break
+        }
+        case 6: {
+          this.percentil = 32
+          break
+        }
+        case 7: {
+          this.percentil = 35
+          break
+        }
+        case 8: {
+          this.percentil = 50
+          break
+        }
+        case 9: {
+          this.percentil = 55
+          break
+        }
+        case 10: {
+          this.percentil = 67
+          break
+        }
+        default: {
+          this.percentil = 87
+        }
+      }
+    }
+  },
+  created () {
+    this.addPercentil()
   }
 
 }
 </script>
 
 <style scoped>
+
+  .chart {
+    margin: auto;
+    background-color: white;
+    width: 50vw;
+  }
 
 </style>
